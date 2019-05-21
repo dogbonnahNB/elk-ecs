0181bdad179e8357b9b275d875e3d8feb4228acd
David Ogbonnah <davido6593@gmail.com>
2019-05-21 17:00:39 +0100

    Fix variable reference

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ module "aws_launch_configuration" {
 
   image_id             = "${data.aws_ami.amazon_linux_ecs.id}"
   instance_type        = "${var.instance_type}"
-  security_groups      = ["${data.aws_security_group.selected.ids}"]
+  security_groups      = "${data.aws_security_groups.selected.ids}"
   iam_instance_profile = "${aws_iam_instance_profile.instance_profile.id}"
   user_data            = "${file("${path.module}/user-data.sh")}"
 
