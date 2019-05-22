152a2858b0f915bc503fa5b303807b3c46817815
David Ogbonnah <davido6593@gmail.com>
2019-05-22 12:26:13 +0100

    Adding shared volume; setting ulimits, placement constraints

--- service-elasticsearch/main.tf
+++ service-elasticsearch/main.tf
@@ -6,6 +6,25 @@ resource "aws_cloudwatch_log_group" "elasticsearch" {
 resource "aws_ecs_task_definition" "elasticsearch" {
   family = "elasticsearch"
   container_definitions = "${file("service-elasticsearch/service.json")}"
+
+  volume {
+    name = "esdata"
+
+    docker_volume_configuration {
+      scope         = "shared"
+      autoprovision = true
+    }
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [eu-west-2a, eu-west-2b]"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.instance-type =~ t2.*"
+  }
 }
 
 resource "aws_ecs_service" "elasticsearch" {
@@ -13,8 +32,8 @@ resource "aws_ecs_service" "elasticsearch" {
   cluster         = "${var.cluster_id}"
   task_definition = "${aws_ecs_task_definition.elasticsearch.arn}"
 
-  desired_count = 8
+  desired_count = 3
 
   deployment_maximum_percent         = 100
-  deployment_minimum_healthy_percent = 50
+  deployment_minimum_healthy_percent = 66
 }
