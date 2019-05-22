8801977b6a2ee516a36029f12e96ea48679bc312
David Ogbonnah <davido6593@gmail.com>
2019-05-22 19:13:13 +0100

    Amend user-data file

--- user-data.sh
+++ user-data.sh
@@ -3,15 +3,12 @@
 
 #Host config
 sysctl -w vm.max_map_count=262144
-mkdir -p /usr/share/elasticsearch/data/
-chown -R 1000.1000 /usr/share/elasticsearch/data/
 
 # ECS config
-{
-  echo "ECS_CLUSTER=fast-cluster"
-  echo "ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=15m"
-  echo "ECS_IMAGE_CLEANUP_INTERVAL=10m"
-} >> /etc/ecs/ecs.config
+
+echo "ECS_CLUSTER=fast-cluster" >> /etc/ecs/ecs.config
+echo "ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=15m" >> /etc/ecs/ecs.config
+echo "ECS_IMAGE_CLEANUP_INTERVAL=10m" >> /etc/ecs/ecs.config
 
 start ecs
 
