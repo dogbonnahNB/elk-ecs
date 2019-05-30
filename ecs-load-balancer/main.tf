8c928fff34d67251431310e0830e3df1a7bdc542
David Ogbonnah <davido6593@gmail.com>
2019-05-30 13:00:49 +0100

    Change LB and health check port numbers

--- ecs-load-balancer/main.tf
+++ ecs-load-balancer/main.tf
@@ -3,6 +3,16 @@ resource "aws_lb_target_group" "ecs-application-lb-tg" {
   port     = "${var.application_port}"
   protocol = "HTTP"
   vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    path = "${var.health_check_path}"
+    port = "${var.health_check_port}"
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
 }
 
 resource "aws_lb" "ecs-application-lb" {
